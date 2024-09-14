e9f5d61650d9894452a9407af308bcf926786e6e
kinghayzed <yekeen28hayzed10@gmail.com>
2024-09-14 19:18:47 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <Navbar/>
         {/* <Header/> */}
         <Routes>
+          <Route path="/" element={<Home/>}/>
           <Route path="/shop" element={<Shop/>}/>
           <Route path="/cart" element={<Cart/>}/>
           <Route path='/home' element={<Home/>}/>
